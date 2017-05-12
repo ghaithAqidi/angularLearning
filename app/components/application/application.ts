e3b5e27b9a910265b1b27cdbf8517a87e43f8201
ghaithAqidi <ghaithammar2008@gmail.com>
2017-05-12 21:05:41 +0200

    initial commit, working on chapter 3 exercises (routing)

---
+++ app/components/application/application.ts
@@ -0,0 +1,15 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'auction-application', // <1>
+  templateUrl: 'app/components/application/application.html', // <3>
+  styleUrls: ['app/components/application/application.css'], // <4>
+  encapsulation:ViewEncapsulation.None
+})
+
+export default class ApplicationComponent {
+
+}
+
+
+
---
+++ app/components/home/home.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import Productservice = require("../../services/product-service");
+import { Product, ProductService } from '../../services/product-service';
+
+
+@Component({
+	selector: 'auction-home-page',
+	styleUrls: ['/home.css'],
+	templateUrl: 'home.html'
+})
+
+export default class HomeComponent {
+	products: Product[] = [];
+
+	constructor(private productService: ProductService) {
+		this.products = this.productService.getProducts();
+	}
+}
